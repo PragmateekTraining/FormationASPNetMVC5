95b03a73d18cb3c52612eabdb8c81d99efbe7adf
Pragmateek <postmaster@pragmateek.com>
2014-05-03 14:50:55 +0200

    Basic routing project

---
+++ FormationASPNetMVC5/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FormationASPNetMVC5
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            // routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // routes.IgnoreRoute("Content/{file}.json");
+
+            /*routes.MapRoute(
+                name: "CNF",
+                url: "ChuckNorrisFacts/{language}",
+                defaults: new { controller = "ChuckNorrisFacts", action = "Get" }
+            );*/
+        }
+    }
+}
---
+++ FormationASPNetMVC5/Controllers/ChuckNorrisFactsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using io = System.IO;
+
+namespace FormationASPNetMVC5.Controllers
+{
+    public class ChuckNorrisFactsController : Controller
+    {
+        //
+        // GET: /ChuckNorrisFacts/{language}
+        public ActionResult Get(string language)
+        {
+            string basePath = Server.MapPath("~");
+
+            string filePath = string.Format("{0}/Content/CNF_{1}.json", basePath, language);
+
+            if (!io.File.Exists(filePath))
+            {
+                return Content("");
+            }
+
+            return Content(io.File.ReadAllText(filePath));
+        }
+	}
+}
