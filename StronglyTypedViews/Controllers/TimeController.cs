ad3ca8bb1cbd8834ba1b9ae713f5e81cd28fa79d
Pragmateek <training@pragmateek.com>
2014-10-19 19:07:28 +0200

    Add strongly typed view sample

---
+++ StronglyTypedViews/Controllers/TimeController.cs
@@ -0,0 +1,31 @@
+﻿using StronglyTypedViews.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StronglyTypedViews.Controllers
+{
+    public class TimeController : Controller
+    {
+        // GET: Time
+        public ActionResult Index()
+        {
+            DateTime now = DateTime.Now;
+
+            ViewBag.Year = now.Year;
+            ViewBag.Month = now.Month;
+            ViewBag.Day = now.Day;
+
+            TimeModel model = new TimeModel
+            {
+                Year = now.Year,
+                Month = now.Month,
+                Day = now.Day
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ StronglyTypedViews/Models/TimeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StronglyTypedViews.Models
+{
+    public class TimeModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+    }
+}
